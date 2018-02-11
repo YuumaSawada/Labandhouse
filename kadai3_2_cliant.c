0a70eb4d044b19a9420379cf9cf2fa44a68e285b
YuumaSawada <190326m@ugs.kochi-tech.ac.jp>
2018-02-11 19:12:52 +0900

    program complete report not complete

--- kadai3_2_cliant.c
+++ kadai3_2_cliant.c
@@ -1,67 +1,71 @@
-// 2017/9/24
-// 澤田優真
-// サーバへメッセージを送信するクライアントプロセス
-// ipアドレスは[ip addr]で調べる(分からない場合)
+// 2018/2/11
+// 1190326 澤田優真
+// サーバへメッセージを送信するクライアントプログラム
 
-#include <sys/types.h> 
-#include <sys/socket.h> 
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <sys/fcntl.h>
-#include <stdio.h> 
-#include <netinet/in.h> 
-#include <arpa/inet.h> 
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
 #include <netdb.h>
-#include <unistd.h> 
+#include <unistd.h>
 #include <stdlib.h>
 
+static int data_buf = 256;
+static int message_buf = 256;
 
 int main(int argc, char *argv[]){
   int sockfd;
   struct sockaddr_in client_addr;
-  char *data[256];
-  char *message[256];
-
+  char data[data_buf];
+  char message[message_buf];
   if(argc != 3){
-    printf("IPアドレス,ポート番号　の順に引数を正しく指定してください\n"); //ip=172.21.34.109?
-  }
-  
-  printf("文字を入力してください\n");     
-  scanf("%s",data);
-  
-  //ソケットを生成 
-  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    perror("client: socket");
+    printf("IPアドレス,ポート番号　の順に引数を正しく指定してください\n");
     exit(1);
-
   }
 
+  //ソケットを生成
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("socket");
+    exit(1);
+  }
   //構造体にサーバの情報を設定
   client_addr.sin_family = AF_INET;
   client_addr.sin_addr.s_addr = inet_addr(argv[1]);
-  client_addr.sin_port = atoi(argv[2]);
-  
+  client_addr.sin_port = htons(atoi(argv[2]));
   //サーバへ接続
-  if (connect(sockfd, (struct sockaddr *)&client_addr, 
-	      sizeof(client_addr)) < 0) {
-    perror("client: connect");
+  if (connect(sockfd, (struct sockaddr *)&client_addr,sizeof(client_addr)) != 0){
+    perror("connect");
     close(sockfd);
     exit(1);
   }
-  
+  printf("文字を入力してください\n");
+  fgets(data, data_buf, stdin);
+  int i = 0;
+  while(1) {
+    if(data[i] == '\n') {
+      data[i] = '\n';
+      data[i+1] = '\0';
+      break;
+    }
+    i++;
+  }
   //メッセージ送信
-  if(write(sockfd, data, 256) < 0){
-    perror("no send");
+  if(write(sockfd, data, data_buf) < 0){
+    perror("write");
     exit(1);
   }
-
   //サーバからのメッセージ受信
-  if(read(sockfd, message, 256) < 0){
-    perror("no read");
+  if(read(sockfd, message, message_buf) < 0){
+    perror("read");
     exit(1);
   }
   printf("%s\n",message);
   if(close(sockfd) < 0){
-    perror("no close");
+    perror("close");
     exit(1);
-  } 
-  exit(0);
+  }
+  return 0;
 }
--- kadai3_2_server.c
+++ kadai3_2_server.c
@@ -1,6 +1,6 @@
-// 2017/9/24
-// 澤田優真
-// クライアントから送信されたメッセージを表示するサーバプロセス
+// 2018/2/11
+// 1190326 澤田優真
+// クライアントから送信されたメッセージを表示するサーバプログラム
 
 
 
@@ -10,82 +10,102 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
-#include <stdio.h> 
-#include <unistd.h> 
+#include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
+
+static int data_buf = 256;
+static int message_buf = 256;
+static int port = 10000;
+
+void waiting(int sign){
+  pid_t pids;
+  int sig;
+  while((pids=waitpid(-1,&sig,WNOHANG))>=0){
+    printf("wait for %d \n",pids);
+  }
+}
 
 int main(int argc, char *argv[]){
 
   int sockfd;
+  int port_num;
   int new_sockfd;
-  int writer_len;
-  char data[256];
-  char message[256] = "accept OK!! \n";
-  struct sockaddr_in reader_addr; 
+  unsigned writer_len;
+  char data[data_buf];
+  char message[256] = "Thanks for message!!\n";
+  struct sockaddr_in reader_addr;
   struct sockaddr_in writer_addr;
+  pid_t pid;
 
-  if(argc != 2){
+  //message="accept OK!! \n";
+
+  if(argc > 2 || argc == 0 ){
     printf("ポート番号を正しく指定してください\n");
+    exit(1);
+  }else if(argc == 1){
+    printf("ポート番号が指定されなかったため, 10000に設定します\n");
+    port_num = port;
+  }else{
+    port_num = atoi(argv[1]);
   }
 
   /* ソケットの生成 */
 
   if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    perror("reader: socket");  }
-
-
-
-  /* 通信ポート・アドレスの設定 */
-
-  reader_addr.sin_family = AF_INET;
-  reader_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  reader_addr.sin_port = atoi(argv[1]);
-
-  printf("起動\n");
-
-  /* ソケットにアドレスを結びつける */
-
-  if (bind(sockfd, (struct sockaddr *)&reader_addr, sizeof(reader_addr)) < 0) {
-    perror("reader: bind");
+    perror("socket");
     exit(1);
   }
-
+  /* 通信ポート・アドレスの設定 */
+  reader_addr.sin_family = AF_INET;
+  reader_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  reader_addr.sin_port = htons(port_num);
+  printf("Start-up\n");
+  /* ソケットにアドレスを結びつける */
+  if (bind(sockfd, (struct sockaddr *)&reader_addr, sizeof(reader_addr)) < 0) {
+    perror("bind");
+    exit(1);
+  }
   /* コネクト要求をいくつまで待つかを設定 */
-
   if (listen(sockfd, 5) < 0) {
-    perror("reader: listen");
+    perror("listen");
     close(sockfd);
     exit(1);
   }
-
-
- 
+  signal(SIGCHLD,waiting);
   while(1){
      /* コネクト要求を待つ */
     printf("waiting accept\n");
-    if ((new_sockfd = accept(sockfd,(struct sockaddr *)&writer_addr, &writer_len)) < 0) {
-      perror("reader: accept");
+    if((new_sockfd = accept(sockfd,(struct sockaddr *)&writer_addr, &writer_len)) < 0){
+      perror("accept");
       exit(1);
-    }
-
-    //送られてきたメッセージを受信
-    if(read(new_sockfd, &data, 256) < 0){
-      perror("no read");
-      exit(1);
-    }
-
-    printf("message is 「%s」 \n", data);
-
-    //受信完了のメッセージをクライアントへ送信
-    if(write(new_sockfd, &message, 256) < 0){
-      perror("no send");
-      exit(1);
-    }
-    
-    //終了
-    if(close(new_sockfd)< 0){
-      perror("no close");
-      exit(1);
+    }else{
+      pid = fork();
+      if(pid==0){
+        //送られてきたメッセージを受信
+        printf("child start\n");
+        if(read(new_sockfd, &data, data_buf) < 0){
+          perror("read");
+          exit(1);
+        }else{
+          printf("Client : %s", data);
+        }
+        //受信完了のメッセージをクライアントへ送信
+        if(write(new_sockfd, &message, message_buf) < 0){
+          perror("send");
+          exit(1);
+        }
+        //終了
+        if(close(new_sockfd)< 0){
+          perror("close");
+          exit(1);
+        }
+      }else{
+        if(close(new_sockfd)< 0){
+          perror("close");
+          exit(1);
+        }
+      }
     }
   }
   return 0;
